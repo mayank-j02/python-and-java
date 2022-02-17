406b1ae1bae0e5354b7080c69f43ae337818ae59
Mayank Jain <77893940+mayank-j02@users.noreply.github.com>
2022-02-17 18:06:39 +0530

    Update arduino.ino

--- Potentiometer Binary/1 Potentiometer/arduino.ino
+++ Potentiometer Binary/1 Potentiometer/arduino.ino
@@ -1,18 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int analogValue = analogRead(A2);
 
   byte b1 = analogValue / 256;
   byte b2 = analogValue % 256;
 
-  //byte b[] = {b1, b2};
+  byte b[] = {b1, b2};
 
-  Serial.write(b1);
-  Serial.write(b2);
-
+  Serial.write(b, 2);
 }
